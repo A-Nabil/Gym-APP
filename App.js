435e3adb1a3cfc2237c22a10c18cb560fdec7f3e
Ahmed Nabil <Ahmed.Nabil@apoteket.se>
2022-08-14 22:05:14 +0200

    Fix App Navigation

--- App.js
+++ App.js
@@ -1,52 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import Card from './components/Card';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 
-export default function App() {
+export default function App({ navigation }) {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-      screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-
-            if (route.name === 'Home') {
-              iconName = focused
-                ? 'home'
-                : 'home';
-            } else if (route.name === 'Profile') {
-              iconName = focused ? 'user' : 'user';
-            }
-
-            // You can return any component that you like here!
-            return <Icon name={iconName} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: 'tomato',
-          tabBarInactiveTintColor: 'gray',
-        })}
-      >
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
-      </Tab.Navigator>
+        <Stack.Navigator>
+    <Stack.Screen name="Home" component={TabNavigator}  options={{ headerShown: false }}/>
+    <Stack.Screen name="Settings" component={SettingsScreen} />
+  </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-function HomeScreen() {
+function TabNavigator()
+{
+  return(
+    <Tab.Navigator
+    screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'HomeTab') {
+            iconName = focused
+              ? 'home'
+              : 'home';
+          } else if (route.name === 'Profile') {
+            iconName = focused ? 'user' : 'user';
+          }
+
+          // You can return any component that you like here!
+          return <Icon name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
+      })}
+    >
+      <Tab.Screen name="HomeTab" component={HomeScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />     
+    </Tab.Navigator>
+  );
+}
+
+const HomeScreen =({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text>Home screen</Text>
-      <Card style={styles.card}>
-          <Text style={styles.sectionTitle}>Basic CardView Example</Text>
-        </Card>
-        <Card style={styles.card}>
-          <Text style={styles.sectionTitle}>Basic CardView Example</Text>
-        </Card>
+      <TouchableOpacity 
+            onPress={() =>{
+              navigation.navigate('Settings');
+            }
+             
+              }
+
+       style={styles.card}>
+        </TouchableOpacity>
     </View>
   );
 }
@@ -58,6 +70,18 @@ function ProfileScreen() {
     </View>
   );
 }
+
+function SettingsScreen() {
+  return (
+    <View >
+      <Text>settings</Text>
+    </View>
+  );
+}
+
+const TestScreen = ({ navigation, route }) => {
+  return <Text>This is {route.params.name}'s profile</Text>;
+};
 
 const styles = StyleSheet.create({
   container: {
