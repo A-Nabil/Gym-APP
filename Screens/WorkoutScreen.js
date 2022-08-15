85bdba8bde3b557c8873d8b00d9cfaca5ea781dd
Ahmed Nabil <Ahmed.Nabil@apoteket.se>
2022-08-15 19:58:22 +0200

    Add horizontal items and round buttons

--- Screens/WorkoutScreen.js
+++ Screens/WorkoutScreen.js
@@ -1,10 +1,37 @@
-import { View, Text } from 'react-native';
+import { View, Text,ScrollView,StyleSheet, TouchableOpacity } from 'react-native';
 
 
 export function WorkOutsScreen()  {
     return (
       <View >
-        <Text>settings</Text>
+
+        <ScrollView horizontal>
+<TouchableOpacity  style={styles.roundButton1}></TouchableOpacity>
+<TouchableOpacity  style={styles.roundButton1}></TouchableOpacity>
+<TouchableOpacity  style={styles.roundButton1}></TouchableOpacity>
+<TouchableOpacity  style={styles.roundButton1}></TouchableOpacity>
+<TouchableOpacity  style={styles.roundButton1}></TouchableOpacity>
+
+</ScrollView>
       </View>
     );
   }
+
+  // Styles
+const styles = StyleSheet.create({
+    screen: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    roundButton1: {
+      width: 80,
+      height: 80,
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: 10,
+      borderRadius: 100,
+      backgroundColor: 'blue',
+      margin: 10
+    },
+  });
